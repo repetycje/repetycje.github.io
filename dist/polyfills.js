ec03c29ce473436e0692c382b9f4b2d17b58df93
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-04-13 20:27:41 +0200

    dist folder

--- dist/polyfills.js
+++ dist/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ulano\source\repos\repetycje.github.io\repetycje\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\ulano\source\repos\repetycje.github.io\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
