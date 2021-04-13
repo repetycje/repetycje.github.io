100a3891fe8712c51ea00564dec31a29334263e5
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-04-13 20:27:41 +0200

    dist folder

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\ulano\source\repos\repetycje.github.io\repetycje\src\main.ts */"zUnb");
+module.exports = __webpack_require__(/*! C:\Users\ulano\source\repos\repetycje.github.io\src\main.ts */"zUnb");
 
 
 /***/ }),
