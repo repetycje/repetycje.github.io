bc406881a9305f03ccfc9e054b5d0784d0c088da
Karol Ulanowski <ulanowskikarol@gmail.com>
2021-04-13 20:44:13 +0200

    working on bot

---
+++ src/models/bot.ts
@@ -0,0 +1,17 @@
+export class Bot {
+    private _alphabet: string;
+    private _word: string;
+
+    constructor(alphabet: string) {
+        this._alphabet = alphabet;
+        this._word = "";
+    }
+
+    public makeMove(char:string): string {
+        this._word+=char;
+        const choice = this._alphabet[0];
+        
+        return choice;
+
+    }
+}
